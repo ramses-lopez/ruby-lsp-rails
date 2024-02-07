0c406427abd4109398d1ae4e4912f65601e7290e
Andy Waite <andyw8@users.noreply.github.com>
2024-02-07 11:54:31 -0500

    wip

--- lib/ruby_lsp/ruby_lsp_rails/runner_client.rb
+++ lib/ruby_lsp/ruby_lsp_rails/runner_client.rb
@@ -11,15 +11,16 @@ module RubyLsp
 
       sig { void }
       def initialize
-        @stdin = T.let(@stdin, T.nilable(IO))
-        @stdout = T.let(@stdout, T.nilable(IO))
-        @stderr = T.let(@stderr, T.nilable(IO))
-        @wait_thread = T.let(@stdin, T.untyped)
-        @stdin, @stdout, @stderr, @wait_thread = Open3.popen3(
+        stdin, stdout, stderr, wait_thread = Open3.popen3(
           "bin/rails",
           "runner",
           "#{__dir__}/server.rb",
         )
+        @stdin = T.let(stdin, IO)
+        @stdout = T.let(stdout, IO)
+        @stderr = T.let(stderr, IO)
+        # TODO: clean up T.must calls
+        @wait_thread = T.let(wait_thread, Thread)
         @stdin.binmode # for Windows compatibility
         @stdout.binmode # for Windows compatibility
       end
@@ -38,19 +39,24 @@ module RubyLsp
 
         hash[:params] = params if params
         json = hash.to_json
-        # need to fix upstream to accept multiple args
-        T.unsafe(@stdin).write("Content-Length: #{json.length}\r\n\r\n", json)
+        # need to fix upstream to accept multiple args (in Sorbet)?
+        @stdin.write("Content-Length: #{json.length}\r\n\r\n" + json)
       end
 
       sig { params(request: T.untyped).returns(T::Hash[String, String]) }
-      def read_response(request)
-        Timeout.timeout(5) do
-          headers = T.must(@stdout).gets("\r\n\r\n")
-          raw_response = T.must(@stdout).read(T.must(headers)[/Content-Length: (\d+)/i, 1].to_i)
-          JSON.parse(T.must(raw_response), symbolize_keys: true)
-        end
-      rescue Timeout::Error
-        raise "Request #{request} timed out. Is the request returning a response?"
+      def read_response(request) # TODO: request not used
+        # Timeout.timeout(5) do
+        headers = @stdout.gets("\r\n\r\n")
+
+        raw_response = @stdout.read(T.must(headers)[/Content-Length: (\d+)/i, 1].to_i)
+        JSON.parse(T.must(raw_response), symbolize_keys: true)
+        # end
+        # rescue Timeout::Error
+        #   raise "Request #{request} timed out. Is the request returning a response?"
+      rescue => e
+        # warn(e.backtrace)
+        warn(@stderr.read)
+        raise
       end
 
       sig { params(name: String).returns(String) }
@@ -61,10 +67,11 @@ module RubyLsp
       sig { void }
       def shutdown
         send_request("shutdown")
+        sleep(0.01) while @wait_thread.alive?
 
-        T.must(@stdin).close
-        T.must(@stdout).close
-        T.must(@stderr).close
+        @stdin.close
+        @stdout.close
+        @stderr.close
       end
     end
   end
--- lib/ruby_lsp/ruby_lsp_rails/server.rb
+++ lib/ruby_lsp/ruby_lsp_rails/server.rb
@@ -7,23 +7,26 @@ require "json"
 module RubyLsp
   module Rails
     class << self
+      VOID = T.let(Object.new, Object)
+
       extend T::Sig
 
-      sig { params(model_name: String).returns(T.nilable(String)) }
+      sig { params(model_name: String).returns(T.nilable(T::Hash[Symbol, T.untyped])) }
       def resolve_database_info_from_model(model_name)
         const = ActiveSupport::Inflector.safe_constantize(model_name)
         return unless const && const < ActiveRecord::Base
 
         begin
+          # TODO: check if we should be relying on this
           schema_file = ActiveRecord::Tasks::DatabaseTasks.schema_dump_path(const.connection.pool.db_config)
         rescue => e
           warn("Could not locate schema: #{e.message}")
         end
 
-        JSON.dump({
+        {
           columns: const.columns.map { |column| [column.name, column.type] },
           schema_file: schema_file,
-        })
+        }
       end
 
       sig { void }
@@ -34,21 +37,34 @@ module RubyLsp
         running = T.let(true, T::Boolean)
 
         while running
-          headers = $stdin.gets("\r\n\r\n")
-          request = $stdin.read(headers[/Content-Length: (\d+)/i, 1].to_i)
+          begin
+            warn("*** in begin")
+            headers = $stdin.gets("\r\n\r\n")
+            warn("no headers") unless headers
+            request = $stdin.read(headers[/Content-Length: (\d+)/i, 1].to_i)
 
-          json = JSON.parse(request, symbolize_names: true)
-          request_method = json.fetch(:method)
-          params = json[:params]
+            json = JSON.parse(request, symbolize_names: true)
+            request_method = json.fetch(:method)
+            params = json[:params]
 
-          response_json = nil
-          case request_method
-          when "shutdown"
-            running = false
-          when "models"
-            model_name = params.fetch(:name)
-            response_json = RubyLsp::Rails.resolve_database_info_from_model(model_name).to_json
+            response = case request_method
+            when "shutdown"
+              running = false
+              VOID
+            when "models"
+              model_name = params.fetch(:name)
+              RubyLsp::Rails.resolve_database_info_from_model(model_name)
+            else
+              VOID
+            end
+            next if response == VOID
+
+            warn(response)
+            response_json = JSON.dump(response) # .to_json
             $stdout.write("Content-Length: #{response_json.length}\r\n\r\n#{response_json}")
+          rescue => e
+            warn(e.backtrace)
+            raise
           end
         end
       end
